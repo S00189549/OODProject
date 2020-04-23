5d20b29dd9a0ec31d03e8c7a35fbc513155cfa4c
Adil Rouibet <46888737+S00189549@users.noreply.github.com>
2020-04-23 03:44:38 +0100

    Database Relationships set up

--- OOD project/OOD project/obj/Debug/OOD project_Content.g.cs
+++ OOD project/OOD project/obj/Debug/OOD project_Content.g.cs
@@ -10,5 +10,7 @@
 
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("exercisedatabase.mdf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("exercisedatabase_log.ldf")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("model1.context.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("model1.tt")]
 
 
